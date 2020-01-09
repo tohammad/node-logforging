274648ff5f6391477dd30c2f68113d36de1363b0
Hammad Mustafa <hammad.mustafa@itpeoplecorp.com>
2020-01-09 13:16:17 +0530

    log forging

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var http = require('http');
 var url = require('url');
+const log4js = require('log4js');
+var logger = log4js.getLogger('server');
+logger.level = 'debug';
 
+function cleanLogString1(logString) {
+    let clean = logString.replace( '\n', '_' ).replace( '\r', '_' );
+    return clean;
+}
+function cleanLogString2(logString) {
+    let clean = logString.replace(/[\n\r]+/g, '_');
+    return clean;
+}
 function listener(request, response){
   var val = url.parse(request.url, true)['query']['val'];
   if (isNaN(val)){
-    console.log("INFO: Failed to parse val = " + val);
+    logger.debug("INFO: Failed to parse val = " + val);
+    logger.debug("INFO: Failed to parse val = " + cleanLogString1(val));
+    logger.debug("INFO: Failed to parse val = " + cleanLogString2(val));
   }
 }
 http.createServer(listener).listen(8080);
